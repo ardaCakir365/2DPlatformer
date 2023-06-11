fdd1e9d32e7957b0338da68b8895cb1b72013329
ardaCakir365 <ardacakir365@gmail.com>
2023-06-11 17:34:11 +0300

    21 files commited
    21 files commited

--- Assets/Scripts/Cheries.cs
+++ Assets/Scripts/Cheries.cs
@@ -7,9 +7,12 @@ using TMPro;
 public class Cheries : MonoBehaviour
 {
     int CheeryCount=0;
+    int BananaCount=0;
 
     [SerializeField]
     public TextMeshProUGUI cherryText;
+    public TextMeshProUGUI bananaText;
+
     // Start is called before the first frame update
     void Start(){
       
@@ -18,10 +21,20 @@ public class Cheries : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.tag == "Cherry"){
 
-            Debug.Log("kod çalştı");
+            Debug.Log("cherry toplandı");
             cherryText.text = "cheeries "+ CheeryCount;
             CheeryCount++;
+            Destroy(collision.gameObject); 
+        }
+        if(collision.gameObject.tag == "Banana"){
+
+            Debug.Log("Banana toplandı!");
+            bananaText.text = "banana "+ CheeryCount;
+            BananaCount++;
             Destroy(collision.gameObject);
         }
+
     }
+
+
 }
