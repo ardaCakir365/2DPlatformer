8a0ffa5e2cdab67749cd812089665025f42ba069
Derya <48880052+deryaguner@users.noreply.github.com>
2023-06-22 01:58:54 +0300

    Enemy mushroom added

---
+++ Assets/Scripts/GiveDamage.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveDamage : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+
+
+}
