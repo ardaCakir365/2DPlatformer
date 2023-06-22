310643005f0d31b0254adb364020a49a579c1bf9
Derya <48880052+deryaguner@users.noreply.github.com>
2023-06-22 22:01:58 +0300

    Level1

--- Assets/Scripts/GiveDamage.cs
+++ Assets/Scripts/GiveDamage.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GiveDamage : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class GiveDamage : MonoBehaviour
         if (collision.transform.CompareTag("Player"))
         {
             Destroy(collision.gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
